f09c04da2acc293c6fe6e68f814ff1d25245fe42
jose <jole696@vidalibarraquer.net>
2026-06-09 22:13:23 +0200

    fix: update symlink path for Proton GE compatibility tool in Steam configuration

--- nix-config/modules/gaming/steam.nix
+++ nix-config/modules/gaming/steam.nix
@@ -3,9 +3,7 @@
 {
   home.packages = with pkgs; [
     steam
-    proton-ge-bin
   ];
 
-  # Symlink Proton GE into Steam's compatibility tools directory
-  home.file.".local/share/Steam/compatibilitytools.d/GE-Proton".source = pkgs.proton-ge-bin.steamcompattool;
+  home.file.".local/share/Steam/compatibilitytools.d/proton-ge-bin".source = pkgs.proton-ge-bin.steamcompattool;
 }
