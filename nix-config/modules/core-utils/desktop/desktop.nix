f877e3aa59bce8bce2ce66b48d71d1e2205e4eb6
jose <jole696@vidalibarraquer.net>
2026-06-19 18:57:45 +0200

    feat: add motrix to desktop packages list

--- nix-config/modules/core-utils/desktop/desktop.nix
+++ nix-config/modules/core-utils/desktop/desktop.nix
@@ -8,5 +8,6 @@
   # Plantilla para paquetes de programas del módulo desktop
   home.packages = with pkgs; [
     # Agrega tus programas de escritorio aquí (por ejemplo: firefox, discord, etc.)
+    motrix
   ];
 }
